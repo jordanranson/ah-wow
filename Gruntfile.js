3759a9a1ef3c5008c68e35897ab9464c9e4ebfd8
Jordan Ranson <jordan@jordanranson.com>
2014-05-22 10:13:06 -0600

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    paths: ["."],
+                    yuicompress: true
+                },
+                files: {
+                    "./app/public/style.css": "./less/master.less"
+                }
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions']
+            },
+            dist: {
+                files: {
+                    './app/public/style.css': ['./app/public/style.css']
+                }
+            }
+        },
+        watch: {
+            files: "./less/*",
+            tasks: ["less", "autoprefixer"]
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default','watch');
+    grunt.registerTask('build',['less','autoprefixer']);
+};
